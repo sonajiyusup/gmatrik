aff7417a68e9b52cb32390b038b9bf282285783d
YodiYanwar <yodieys_yanwar@windowslive.com>
2018-04-22 19:51:03 +0700

    create dashboard + sidebar page on admin role

---
+++ role/admin/dashboard.php
@@ -0,0 +1,63 @@
+<?php 
+
+  include 'functions.php';
+  $tu = totalUser();
+  $tam = totalAdminMatrik();
+  $tp = totalPembina();
+  $tm = totalMahasiswa();
+   
+?>
+
+<div class="container-fluid">
+            <div class="block-header">
+                <h2>DASHBOARD</h2>
+            </div>
+            <!-- Widgets -->
+            <div class="row clearfix">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <div class="info-box-2 bg-blue">
+                        <div class="icon">
+                            <a href="?page=users"><i class="material-icons">account_box</i></a>
+                        </div>
+                        <div class="content">
+                            <div class="text">JUMLAH PENGGUNA SISTEM</div>
+                            <div class="number count-to" data-from="0" data-to="125" data-speed="1000" data-fresh-interval="20"><?php foreach($tu as $totalUser){ echo $totalUser; }?></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <div class="info-box-2 bg-green">
+                        <div class="icon">
+                            <a href="?page=adminmatrik"><i class="material-icons">supervisor_account</i></a>
+                        </div>
+                        <div class="content">
+                            <div class="text">JUMLAH ADMIN MATRIKULASI</div>
+                            <div class="number count-to" data-from="0" data-to="125" data-speed="1000" data-fresh-interval="20"><?php foreach($tam as $totalAM){ echo $totalAM; }?></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <div class="info-box-2 bg-red">
+                        <div class="icon">
+                            <a href="?page=pembina"><i class="material-icons">gavel</i></a>
+                        </div>
+                        <div class="content">
+                            <div class="text">JUMLAH PEMBINA MAHASISWA</div>
+                            <div class="number count-to" data-from="0" data-to="125" data-speed="1000" data-fresh-interval="20"><?php foreach($tp as $totalPembina){ echo $totalPembina; }?></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <div class="info-box-2 bg-red">
+                        <div class="icon">
+                            <a href="?page=mahasiswa"><i class="material-icons">gavel</i></a>
+                        </div>
+                        <div class="content">
+                            <div class="text">JUMLAH MAHASISWA</div>
+                            <div class="number count-to" data-from="0" data-to="125" data-speed="1000" data-fresh-interval="20"><?php foreach($tm as $totalMahasiswa){ echo $totalMahasiswa; }?></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #END# Widgets -->
+</div>
---
+++ role/admin/sidebar.php
@@ -0,0 +1,197 @@
+<aside id="leftsidebar" class="sidebar">
+            <!-- User Info -->
+            <div class="user-info">
+                <div class="image">
+                    <img src=<?php echo "assets/img/user/".$_SESSION['ava']; ?> width="48" height="48" alt="User" />
+                </div>
+                <div class="info-container">
+                    <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION['nama']; ?></div>
+                    <div class="email"><?php echo $_SESSION['rolename']; ?></div>
+                    <div class="btn-group user-helper-dropdown">
+                        <i class="material-icons" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">keyboard_arrow_down</i>
+                        <ul class="dropdown-menu pull-right">
+                            <li><a href="?page=profil" style="color:#3C8DBC;"><i class="material-icons">person</i>Profil</a></li>
+                            <li role="seperator" class="divider"></li>
+                            <li><a href="#ModalLogout" data-toggle='modal' style='color:#DD4B39;'><i class="material-icons">power_settings_new</i>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- #User Info -->
+            <!-- Menu -->
+            <div class="menu">
+                <ul class="list">
+                    <li class="header">HOME</li>
+                    <li <?php 
+                          if ($_SERVER['REQUEST_URI'] == '/gmatrik/index.php') {
+                             echo "class='active'";
+                          }
+                        ?>
+                    ><a href="index.php">
+                            <i class="material-icons">dashboard</i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="header">PENGGUNA</li>
+                    <li <?php 
+                                  if (isset($_GET['page'])) {
+                                    if ($_GET['page'] == 'users' || $_GET['page'] == 'pimpinan' || $_GET['page'] == 'adminmatrik' || $_GET['page'] == 'pembina' || $_GET['page'] == 'mahasiswa') {
+                                      echo "class='active'";
+                                    }
+                                  }
+                                ?>
+                            >
+                        <a href="javascript:void(0);" class="menu-toggle">
+                            <i class="material-icons">account_box</i>
+                            <span>Manajemen Pengguna</span>
+                        </a>
+                        <ul class="ml-menu">
+                            <li <?php 
+                                  if (isset($_GET['page'])) {
+                                    if ($_GET['page'] == 'users') {
+                                      echo "class='active'";
+                                    }
+                                  }
+                                ?>
+                            ><a href="?page=users">Semua</a>
+                            </li>
+                        </ul>
+                        <ul class="ml-menu">
+                            <li <?php 
+                                  if (isset($_GET['page'])) {
+                                    if ($_GET['page'] == 'pimpinan') {
+                                      echo "class='active'";
+                                    }
+                                  }
+                                ?>
+                            ><a href="?page=pimpinan">Pimpinan Matrikulasi</a>
+                            </li>
+                        </ul>
+                        <ul class="ml-menu">
+                            <li <?php 
+                                  if (isset($_GET['page'])) {
+                                    if ($_GET['page'] == 'adminmatrik') {
+                                      echo "class='active'";
+                                    }
+                                  }
+                                ?>
+                            ><a href="?page=adminmatrik">Admin Matrikulasi</a>
+                            </li>
+                        </ul>
+                        <ul class="ml-menu">
+                            <li <?php 
+                                  if (isset($_GET['page'])) {
+                                    if ($_GET['page'] == 'pembina') {
+                                      echo "class='active'";
+                                    }
+                                  }
+                                ?>
+                            ><a href="?page=pembina">Pembina Matrikulasi</a>
+                            </li>
+                        </ul>
+                        <ul class="ml-menu">
+                            <li <?php 
+                                  if (isset($_GET['page'])) {
+                                    if ($_GET['page'] == 'mahasiswa' || $_GET['page'] == 'mahasiswadetails' || $_GET['page'] == 'editmahasiswa') {
+                                      echo "class='active'";
+                                    }
+                                  }
+                                ?>
+                            ><a href="?page=mahasiswa">Mahasiswa</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="header">PROFIL</li>
+                    <li <?php 
+                          if (isset($_GET['page'])) {
+                            if ($_GET['page'] == 'profil' || $_GET['page'] == 'editprofil') {
+                              echo "class='active'";
+                            }
+                          }
+                        ?>
+                    ><a href="javascript:void(0);" class="menu-toggle">
+                            <i class="material-icons">supervisor_account</i>
+                            <span>Profil</span>
+                        </a>
+                        <ul class="ml-menu">
+                            <li <?php 
+                                  if (isset($_GET['page'])) {
+                                    if ($_GET['page'] == 'profil' || $_GET['page'] == 'editprofil') {
+                                      echo "class='active'";
+                                    }
+                                  }
+                                ?>
+                            ><a href="?page=profil">Profil Saya</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!-- #Menu -->
+            <!-- Footer -->
+            <div class="legal">
+                <div class="copyright">
+                    &copy; 2016 - 2017 <a href="javascript:void(0);">AdminBSB - Material Design</a>.
+                </div>
+                <div class="version">
+                    <b>Version: </b> 1.0.5
+                </div>
+            </div>
+            <!-- #Footer -->
+</aside>
+
+<section class="content">
+
+    <?php  
+        if (isset($_GET['page'])) {
+            if ($_GET['page'] == 'mahasiswa') {
+              include 'mahasiswa/mahasiswa.php';
+            } else if ($_GET['page'] == 'pembina') {
+              include 'pembina/pembina.php';
+            } else if ($_GET['page'] == 'pembinadetails') {
+              include 'pembina/pembina_detail.php';
+            } else if ($_GET['page'] == 'editpembina') {
+              include 'pembina/pembina_edit.php';
+            } else if ($_GET['page'] == 'profil') {
+              include 'profil.php';
+            } else if ($_GET['page'] == 'editprofil') {
+              include 'profil_edit.php';
+            } else if ($_GET['page'] == 'mahasiswadetails') {
+              include 'mahasiswa/mahasiswa_detail.php';
+            } else if ($_GET['page'] == 'editmahasiswa') {
+              include 'mahasiswa/mahasiswa_edit.php';
+            } else if ($_GET['page'] == 'bypembina') {
+              include 'bypembina.php';
+            } else if ($_GET['page'] == 'bypembinadetail') {
+              include 'bypembinadetail.php';
+            } else if ($_GET['page'] == 'tambahbinaan') {
+              include 'pembina/pembina_tambahbinaan.php';
+            } else if ($_GET['page'] == 'shalat') {
+              include 'shalat/shalat.php';
+            } else if ($_GET['page'] == 'pbentuk') {
+              include 'pelanggaran/pbentuk.php';
+            } else if ($_GET['page'] == 'paksi') {
+              include 'pelanggaran/paksi.php';
+            } else if ($_GET['page'] == 'psanksi') {
+              include 'pelanggaran/psanksi.php';
+            } else if ($_GET['page'] == 'planjut') {
+              include 'pelanggaran/planjut.php';
+            } else if ($_GET['page'] == 'pikhtisar') {
+            include 'pelanggaran/pikhtisar.php';
+          } else if ($_GET['page'] == 'pmaindetail') {
+            include 'pelanggaran/pikhtisar_detail.php';
+          } else if ($_GET['page'] == 'pbentukdetail') {
+            include 'pelanggaran/pbentuk_detail.php';
+          } else if ($_GET['page'] == 'paksidetail') {
+            include 'pelanggaran/paksi_detail.php';
+          } else if ($_GET['page'] == 'psanksidetail') {
+            include 'pelanggaran/psanksi_detail.php';
+          } else if ($_GET['page'] == 'planjutdetail') {
+            include 'pelanggaran/planjut_detail.php';
+          } 
+        } else{
+            include 'dashboard.php';
+        }
+
+    ?>
+</section>
