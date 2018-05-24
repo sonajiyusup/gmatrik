d5de456822c2976e9e7cdb6184e80f4b611cf0aa
YodiYanwar <yodi.yanwar@gmail.com>
2018-05-24 11:41:46 +0700

    update chartjs on shalat ikhtisar

--- assets/js/chartjs.js
+++ assets/js/chartjs.js
@@ -11,14 +11,28 @@ function getChartJs(type) {
             data: {
                 labels: ["1", "2", "3", "4", "5", "6", "7", "8"],
                 datasets: [{
-                    label: "Data Minggu Ini",
+                    label: "Nilai Rata-rata",
                     data: [61, 62, 86, 58, 64, 62, 63, 81],
                     borderColor: 'rgba(0, 188, 212, 0.75)',
                     backgroundColor: 'rgba(0, 188, 212, 0.3)',
                     pointBorderColor: 'rgba(0, 188, 212, 0)',
                     pointBackgroundColor: 'rgba(0, 188, 212, 0.9)',
                     pointBorderWidth: 1
-                }]
+                }, {
+                        label: "Target Jumlah Shalat",
+                        data: [33, 33, 23, 35, 34, 35, 35, 23],
+                        borderColor: 'rgba(233, 30, 99, 0.75)',
+                        pointBorderColor: 'rgba(233, 30, 99, 0)',
+                        pointBackgroundColor: 'rgba(233, 30, 99, 0.9)',
+                        pointBorderWidth: 1
+                    }, {
+                        label: "Jumlah Shalat",
+                        data: [20, 20, 20, 20, 22, 22, 22, 19],
+                        borderColor: 'rgba(154,10,222,0.75)',
+                        pointBorderColor: 'rgba(154,10,222, 0)',
+                        pointBackgroundColor: 'rgba(154,10,222, 0.9)',
+                        pointBorderWidth: 1
+                    }]
             },
             options: {
                 responsive: true,
--- role/adminmatrik/shalat/shalat.php
+++ role/adminmatrik/shalat/shalat.php
@@ -8,7 +8,7 @@
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
                         <div class="header">
-                            <h2>NILAI RATA-RATA PER-PERIODE</h2>
+                            <h2>GRAFIK PRESENSI NILAI RATA-RATA SEMUA MAHASISWA</h2>
                             <ul class="header-dropdown m-r--5">
                                 <li class="dropdown">
                                     <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
