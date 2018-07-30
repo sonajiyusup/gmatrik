731bee217f26596454dfd26ca0ef6a50452e775f
YodiYanwar <yodi.yanwar@gmail.com>
2018-07-30 13:53:16 +0700

    add line chart on shalatiadetail

--- role/adminmatrik/shalat/shalat_iadetail.php
+++ role/adminmatrik/shalat/shalat_iadetail.php
@@ -4,12 +4,81 @@
  ?>
 
   <div class="row clearfix">
-               
+    <!-- Line Chart -->
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                          <h2>
+                          <a href="?page=shalatia" class="btn btn-sm btn-link waves-effect" title="Kembali"><i class="material-icons">arrow_back</i></a>&nbsp;&nbsp;&nbsp;
+                          GRAFIK NILAI PRESENSI MAHASISWA <?php if($jKelamin == 'Akhwat'){echo 'AKHWAT';}else if($jKelamin == 'Ikhwan'){echo 'IKHWAN';} ?></h2>
+                            <small>
+                              Pilih Berdasarkan :
+                              <div class="btn-group">
+                                                    <button type="button" class="btn bg-cyan waves-effect dropdown-toggle btn-sm" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                        <?php 
+                                                          if($jKelamin == 'Akhwat'){echo 'AKHWAT';}else if($jKelamin == 'Ikhwan'){echo 'IKHWAN';}
+                                                        ?>
+                                            <span class="caret"></span>
+                                                    </button>
+                                                    <ul class="dropdown-menu">
+                                                      <li><a href="?page=shalatiadetail&j=Ikhwan">IKHWAN</a></li>
+                                                      <li><a href="?page=shalatiadetail&j=Akhwat">AKHWAT</a></li>
+                                                    </ul>
+                                                </div>
+                            </small>
+                        </div>
+                        <div class="body">
+                            <canvas id="line_chart" height="70"></canvas>
+                        </div>
+                        <script type="text/javascript">
+                          $(function () {
+                              new Chart(document.getElementById("line_chart").getContext("2d"), getChartJs('line'));
+                          });
+
+                          function getChartJs(type) {
+                              var config = null;
+
+                              if (type === 'line') {
+                                  config = {
+                                      type: 'line',
+                                      data: {
+                                          labels: [<?php
+                                                    $dataPeriode = tampilPeriodeShalat();
+                                                    foreach ($dataPeriode as $row){
+                                                     echo '"'.$row['id_periode'].'",';
+                                                    }
+                                                  ?>],
+                                          datasets: [{
+                                              label: "Rata-rata Nilai Shalat",
+                                              data: [<?php
+                                                    $dataNilai = shalatIAByDetail($jKelamin);
+                                                    foreach ($dataNilai as $row){
+                                                     echo '"'.$row['nilai'].'",';
+                                                    }
+                                                  ?>],
+                                              borderColor: 'rgba(0, 188, 212, 0.75)',
+                                              backgroundColor: 'rgba(0, 188, 212, 0.3)',
+                                              pointBorderColor: 'rgba(0, 188, 212, 0)',
+                                              pointBackgroundColor: 'rgba(0, 188, 212, 0.9)',
+                                              pointBorderWidth: 1
+                                          }]
+                                      },
+                                      options: {
+                                          responsive: true,
+                                          legend: false
+                                      }
+                                  }
+                              }
+                              return config;
+                          }                          
+                        </script>
+                    </div>
+                </div>
 
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
                         <div class="header">
-                          <h2>DATA NILAI RATA-RATA PRESENSI MAHASISWA BERDASARKAN <?php echo $jKelamin; ?></h2>
+                          <h2>GRAFIK NILAI PRESENSI MAHASISWA <?php if($jKelamin == 'Akhwat'){echo 'AKHWAT';}else if($jKelamin == 'Ikhwan'){echo 'IKHWAN';} ?></h2>
                         </div>
                         <div class="body">
                           <div class="table-responsive">
