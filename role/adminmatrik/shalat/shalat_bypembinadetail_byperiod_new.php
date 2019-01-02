859f65799444d460d7860da4265b3764b86b4988
YodiYanwar <yodi.yanwar@gmail.com>
2019-01-02 11:58:35 +0700

    update

--- role/adminmatrik/shalat/shalat_bypembinadetail_byperiod_new.php
+++ role/adminmatrik/shalat/shalat_bypembinadetail_byperiod_new.php
@@ -1,6 +1,7 @@
 <?php 
   include 'functions.php';
-  $idPembina = $_GET['id'];
+  $idPembina = $_GET['pm'];
+  $idPeriode = $_GET['pr'];
  ?>
 
 	<div class="row clearfix">
@@ -8,17 +9,7 @@
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="card">
                         <div class="header">
-                          <h2><a href="?page=shalatbpembina" class="btn btn-sm btn-link waves-effect" title="Kembali"><i class="material-icons">arrow_back</i></a>&nbsp;&nbsp;&nbsp;DATA NILAI PRESENSI MAHASISWA BERDASARKAN PEMBINA
-                            <br>
-                              <span class="label bg-cyan"> Jumlah Mahasiswa Binaan : 
-                                    <?php 
-                                      $jmlb = tampilJmlBinaan($idPembina);
-                                        foreach($jmlb as $row){
-                                          echo $row['jmlb'];
-                                        } 
-                                    ?>
-                              </span>
-                          </h2>
+                          <h2><a href="?page=shalatbpembinadetail&id=<?php echo $idPembina; ?>" class="btn btn-sm btn-link waves-effect" title="Kembali"><i class="material-icons">arrow_back</i></a>&nbsp;&nbsp;&nbsp;DATA NILAI PRESENSI MAHASISWA BERDASARKAN PEMBINA</h2>
                         </div>
                         <div class="body">
                           <div class="table-responsive">
@@ -26,19 +17,21 @@
                               <thead>
                                 <tr>
                                   <th>#</th>
-                                  <th>Periode</th>
+                                  <th>NIM</th>
+                                  <th>Nama Mahasiswa</th>
                                   <th>Total</th>
                                 </tr>
                               </thead>
                               <tbody>
                                 <?php 
                                   $no = 1;
-                                  $dataPembina = shalatByPembinaIdNew($idPembina);
+                                  $dataPembina = shalatByPembinaIdNewDetail($idPembina, $idPeriode);
                                   foreach($dataPembina as $row){
                                  ?>
                                 <tr>
                                   <td><?php echo $no; ?></td>
-                                  <td><?php echo '<a href="?page=shalatbpembinadetailnew&pm='.$idPembina.'&pr='.$row['id_periode'].'">'.date('d M Y', strtotime($row['tanggal_dari']))." - ".date('d M Y', strtotime($row['tanggal_sampai'])).'</a>'; ?></td>
+                                  <td><?php echo $row['nim']; ?></td>
+                                  <td><?php echo $row['nama']; ?></td>
                                   <td><?php echo $row['total']; ?></td>
                                 </tr>
                                 <?php $no++; } ?>
